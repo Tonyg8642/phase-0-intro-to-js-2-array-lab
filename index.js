dc1be5d055fe1acc3125b36d10ba3bbaf4e68bb9
Tony Glass <idttip.tg@gmail.com>
2023-08-13 15:15:40 -0700

    completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+    return cats
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+    return cats
+}
+
+function appendCat(name){
+   let copyOfcat = [...cats]
+    copyOfcat.push(name)
+    return copyOfcat
+}
+
+function prependCat(name){
+    let copyOfcat = [...cats]
+    copyOfcat.unshift(name)
+    return copyOfcat
+}
+
+function removeLastCat(){
+    let copyOfcat = [...cats]
+    copyOfcat.pop(name)
+    return copyOfcat
+}
+
+function removeFirstCat(){
+    let copyOfcat = [...cats]
+    copyOfcat.shift()
+    return copyOfcat
+}
+
